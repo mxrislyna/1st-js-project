f39b46f5b5d3abd44c8873679816bc7fccabf24a
mxrislyna <160684475+mxrislyna@users.noreply.github.com>
2026-06-19 18:11:55 -0500

    Organize indentions and add 2 variables

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
+   
     if (randomNumber === 0) {
         return "rock";
     } else if (randomNumber === 1) {
@@ -10,25 +10,25 @@ function getComputerChoice() {
     };
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let input = prompt("Choose\: rock, paper, or scissors?").toLowerCase();
 
-
-    function getHumanChoice() {
-        let input = prompt("Choose\: rock, paper, or scissors?").toLowerCase();
-
-        switch (input) {
-            case "rock":
-                console.log("You picked rock!");
-                break;
-            case "paper":
-                console.log("You picked paper!");
-                break;
-            case "scissors":
-                console.log("You picked scissors!");
-                break;
-            default:
-                console.log("Oops. Please try again.");
+    switch (input) {
+        case "rock":
+            console.log("You picked rock!");
+            break;
+        case "paper":
+            console.log("You picked paper!");
+            break;
+        case "scissors":
+            console.log("You picked scissors!");
+            break;
+        default:
+            console.log("Oops. Please try again.");
         }
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+

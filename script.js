0b21a048aa3820816c0e84332e67135f96f13262
mxrislyna <160684475+mxrislyna@users.noreply.github.com>
2026-06-19 19:17:20 -0500

    Add func capitalize for line 55. Add func playRound with parameter humanChoice and computerChoice. Add conditional for a single round.

--- script.js
+++ script.js
@@ -16,19 +16,49 @@ function getHumanChoice() {
     switch (input) {
         case "rock":
             console.log("You picked rock!");
-            break;
+            return "rock";
         case "paper":
             console.log("You picked paper!");
-            break;
+            return "paper";
         case "scissors":
             console.log("You picked scissors!");
-            break;
+            return "scissors";
         default:
             console.log("Oops. Please try again.");
+            return getHumanChoice();        
         }
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+function capitalize(word){
+    if (!word) return "";
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
 
+function playRound (humanChoice, computerChoice) {
+
+   if (humanChoice === computerChoice) {
+    return "The computer copied you! Try again!"; 
+   } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+    return "You lose! Paper beats Rock!";
+   } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1;
+    return "You lose! Scissors beats Paper!";
+   } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1;
+    return "You lose! Rock beats Scissors!"; 
+   } else {
+    humanScore += 1;
+    return `You Won! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}!`;
+   }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(humanScore);
+console.log(computerScore);
